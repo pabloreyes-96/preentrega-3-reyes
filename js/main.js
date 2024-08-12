e1bc622ce3a863aeeda931dd0fced888987f41e2
pablo <pablojoo96@gmail.com>
2024-08-11 22:54:40 -0300

    se agrega el proyecto

--- js/main.js
+++ js/main.js
@@ -1,8 +1,146 @@
-const formSolicitarViaje = document.querySelector(".form-solicitarViaje");
+const getViajes = () => {
+  fetch("./js/viajes.json")
+    .then((response) => response.json())
+    .then((viajes) => {
+      // aca tengo que llenar la taba con la raid de viajes
+      const tabla = document.getElementById("tabla");
+      tabla.style.display = "block";
+      loadTableData(viajes);
+    });
+};
 
-formSolicitarViaje.addEventListener('submit', e => {
-    e.preventDefault();
-    console.log('formu enviado');
-    /* const nombre = document.querySelector('#nombre').value; */
+function loadTableData(viajes) {
+  const table = document.getElementById("testBody");
+  viajes.forEach((item) => {
+    let row = table.insertRow();
+    let date = row.insertCell(0);
+    date.innerHTML = item.nombre;
+    let name = row.insertCell(1);
+    name.innerHTML = item.precio;
+  });
+}
 
+const btnCart = document.querySelector(".container-cart-icon");
+const containerCartProducts = document.querySelector(
+  ".container-cart-products"
+);
+
+btnCart.addEventListener("click", () => {
+  containerCartProducts.classList.toggle("hidden-cart");
 });
+
+/* ========================= */
+const cartInfo = document.querySelector(".cart-product");
+const rowProduct = document.querySelector(".row-product");
+
+// Lista de todos los contenedores de productos
+const productsList = document.querySelector(".container-items");
+
+// Variable de arreglos de Productos
+let allProducts = [];
+
+const valorTotal = document.querySelector(".total-pagar");
+
+const countProducts = document.querySelector("#contador-productos");
+
+const cartEmpty = document.querySelector(".cart-empty");
+const cartTotal = document.querySelector(".cart-total");
+
+productsList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-add-cart")) {
+    const product = e.target.parentElement;
+
+    const infoProduct = {
+      quantity: 1,
+      title: product.querySelector("h2").textContent,
+      price: product.querySelector("p").textContent,
+    };
+
+    const exits = allProducts.some(
+      (product) => product.title === infoProduct.title
+    );
+
+    if (exits) {
+      const products = allProducts.map((product) => {
+        if (product.title === infoProduct.title) {
+          product.quantity++;
+          return product;
+        } else {
+          return product;
+        }
+      });
+      allProducts = [...products];
+    } else {
+      allProducts = [...allProducts, infoProduct];
+    }
+
+    showHTML();
+  }
+});
+
+rowProduct.addEventListener("click", (e) => {
+  if (e.target.classList.contains("icon-close")) {
+    const product = e.target.parentElement;
+    const title = product.querySelector("p").textContent;
+
+    allProducts = allProducts.filter((product) => product.title !== title);
+
+    console.log(allProducts);
+
+    showHTML();
+  }
+});
+
+// Funcion para mostrar  HTML
+const showHTML = () => {
+  if (!allProducts.length) {
+    cartEmpty.classList.remove("hidden");
+    rowProduct.classList.add("hidden");
+    cartTotal.classList.add("hidden");
+  } else {
+    cartEmpty.classList.add("hidden");
+    rowProduct.classList.remove("hidden");
+    cartTotal.classList.remove("hidden");
+  }
+
+  // Limpiar HTML
+  rowProduct.innerHTML = "";
+
+  let total = 0;
+  let totalOfProducts = 0;
+
+  allProducts.forEach((product) => {
+    const containerProduct = document.createElement("div");
+    containerProduct.classList.add("cart-product");
+
+    containerProduct.innerHTML = `
+            <div class="info-cart-product">
+                <span class="cantidad-producto-carrito">${product.quantity}</span>
+                <p class="titulo-producto-carrito">${product.title}</p>
+                <span class="precio-producto-carrito">${product.price}</span>
+            </div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="icon-close"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        `;
+
+    rowProduct.append(containerProduct);
+
+    total = total + parseInt(product.quantity * product.price.slice(1));
+    totalOfProducts = totalOfProducts + product.quantity;
+  });
+
+  valorTotal.innerText = `$${total}`;
+  countProducts.innerText = totalOfProducts;
+};
--- js/solicitar.js
+++ js/solicitar.js
@@ -1,48 +1,95 @@
 const formSolicitarViaje = document.querySelector(".form-solicitarViaje");
-const nombre = document.getElementById('nombre');
-const ubicacion = document.getElementById('ubicacion');
-const destino = document.getElementById('destino');
-const hora = document.getElementById('horario');
+const nombre = document.getElementById("nombre");
+const celular = document.getElementById("celular");
+const pasajeros = document.getElementById("pasajeros");
+const alertaError = document.querySelector(".alerta-error");
+const alertaExito = document.querySelector(".alerta-exito");
+const contenedorBienvenido = document.getElementById("container");
 
+const nombreL = /^[a-zA-Z0-9\ \-]{2,40}$/;
+const celularL = /^[0-9]{8,10}$/;
+const pasajerosL = /^[1-9]{1}$/;
 
-const nombreL = /^[a-zA-Z0-9\_\-]{2,20}$/;
-const ubicacionL = /^[a-zA-Z0-9-]{2,20}$/;
-const destinoL = /^[a-zA-Z0-9-]{2,20}$/;
-const horaL = /^[0-9]+[:]+[0-9]+[0-9]$/;
+const estadoValidacionCampo = {
+  nombre: false,
+  celular: false,
+  pasajeros: false,
+};
 
-document.addEventListener("DOMContentLoaded",() => {
-    formSolicitarViaje.addEventListener('submit', e => {
-        e.preventDefault();
-        enviarSolicitud();
-    });
-    nombre.addEventListener('input', () => {
-        validarCampo(nombreL, nombre, 'el nombre debe tener mas de 2 a 20 digitos');
-    })
-    ubicacion.addEventListener('input', () => {
-        validarCampo(ubicacionL, ubicacion, 'la ubicacion debe contener la altura exacta');
-    })
-    destino.addEventListener('input', () => {
-        validarCampo(destinoL, destino, 'el destino debe contener la altura exacta ');
-    })
-    hora.addEventListener('input', () => {
-        validarCampo(horaL, hora, 'ingrese el horario como corresponde');
-    })
+document.addEventListener("DOMContentLoaded", () => {
+  formSolicitarViaje.addEventListener("submit", (e) => {
+    e.preventDefault();
+    enviarSolicitud();
+  });
+  nombre.addEventListener("input", () => {
+    validarCampo(nombreL, nombre, "El nombre debe tener mas de 2 a 20 digitos");
+  });
+  celular.addEventListener("input", () => {
+    validarCampo(
+      celularL,
+      celular,
+      "El numero de celular debe contener de 8 a 10 numeros : 1122222332 O 44445555"
+    );
+  });
+  pasajeros.addEventListener("input", () => {
+    validarCampo(
+      pasajerosL,
+      pasajeros,
+      "La cantidad de pasajero debe ser mayor a 0 "
+    );
+  });
 });
-function enviarSolicitud() {
-
-}
 
 function validarCampo(regularExpresion, campo, mensaje) {
-    const validarCampo = regularExpresion.test(campo.value);
-    if(validarCampo){
-        console.log('campo correcto')
-    }else {
-        mostrarAlerta(campo.parentElement,mensaje);
-    }
+  const validarCampo = regularExpresion.test(campo.value);
+  if (validarCampo) {
+    eliminarAlerta(campo.parentElement.parentElement);
+    estadoValidacionCampo[campo.id] = true;
+    campo.parentElement.classList.remove("error");
+    return;
+  }
+  estadoValidacionCampo[campo.id] = false;
+  mostrarAlerta(campo.parentElement.parentElement, mensaje);
+  campo.parentElement.classList.add("error");
 }
 
 function mostrarAlerta(referencia, mensaje) {
-    const alertaDiv = document.createElement('div')
-    alertaDiv.textContent = mensaje;
-    formSolicitarViaje.appendChild(alertaDiv);
+  eliminarAlerta(referencia);
+  const alertaDiv = document.createElement("div");
+  alertaDiv.classList.add("alerta");
+  alertaDiv.textContent = mensaje;
+  referencia.appendChild(alertaDiv);
+}
+
+function eliminarAlerta(referencia) {
+  const alerta = referencia.querySelector(".alerta");
+  if (alerta) {
+    alerta.remove();
+  }
+}
+
+function enviarSolicitud() {
+  if (
+    estadoValidacionCampo.nombre &&
+    estadoValidacionCampo.celular &&
+    estadoValidacionCampo.pasajeros
+  ) {
+    formSolicitarViaje.reset();
+    alertaExito.classList.add("alertaExito");
+    alertaError.classList.remove("alertaError");
+    estadoValidacionCampo.nombre = false;
+    estadoValidacionCampo.celular = false;
+    estadoValidacionCampo.pasajeros = false;
+
+    // primero esperar 2 seg
+    // segundo borrar el formulario
+    contenedorBienvenido.remove();
+
+    // tercero hacer el fecth
+    getViajes();
+    return;
+  }
+
+  alertaExito.classList.remove("alertaExito");
+  alertaError.classList.add("alertaError");
 }
